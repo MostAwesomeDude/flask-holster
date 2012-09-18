db1e63684efa68c315d7b50e84821a1bbfc4a5b8
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-09-18 16:52:54 -0700

    views: Attempt to improve the HTML templater.
    Still not working right. :T

--- flask_holster/views.py
+++ flask_holster/views.py
@@ -1,4 +1,28 @@
-from flask import json
+from flask import json, render_template_string
+
+html_template = """
+<ul>
+{% for k in d recursive %}
+    {% set v = d[k] %}
+    {{ d|pprint }}
+    <li>{{ k }}:
+        {% if v is mapping %}
+        <ul>{{ loop(v) }}</ul>
+        {% else %}
+        {{ v }}
+        {% endif %}
+    </li>
+{% endfor %}
+</ul>
+"""
+
+
+class HTMLTemplate(object):
+
+    def format(self, d):
+        return render_template_string(html_template, d=d)
+
+
 
 class JSONTemplate(object):
 
@@ -15,6 +39,7 @@ class PlainTemplate(object):
 
 
 templates = {
+    "text/html":        HTMLTemplate,
     "application/json": JSONTemplate,
     "text/plain":       PlainTemplate,
 }
