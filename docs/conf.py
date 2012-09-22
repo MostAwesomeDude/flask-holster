16134a266cfabcaf538ee372a11f6f19cdb1f364
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-09-22 12:22:43 -0700

    Add flask-small theme.

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,11 @@
 
 import sys, os
 
+# Flask theming.
+sys.path.append(os.path.abspath("_themes"))
+html_theme_path = ["_themes"]
+html_theme = "flask_small"
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -88,18 +93,6 @@ pygments_style = 'sphinx'
 
 
 # -- Options for HTML output ---------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-html_theme = 'default'
-
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#html_theme_options = {}
-
-# Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
