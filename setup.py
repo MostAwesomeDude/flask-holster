2b1dbb641eebaa8cbf1d0229e7db1cd741b48a49
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-10-09 15:05:58 -0700

    setup: Bump to 0.2.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="Flask-Holster",
-    version="0.1.2",
+    version="0.2",
     url="http://github.com/MostAwesomeDude/flask-holster",
     license="MIT",
     author="Corbin Simpson",
